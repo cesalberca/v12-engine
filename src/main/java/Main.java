bc0b1b3792e3f1dae34d274fe340b113b13a49c6
César Alberca <cesalberca@gmail.com>
2017-02-07 18:15:05 +0100

    Creado session factory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import vista.App;
 
 public class Main {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
     public static void main(String[] args) {
+        final Session session = sessionFactory.openSession();
         App app = new App();
     }
 }
