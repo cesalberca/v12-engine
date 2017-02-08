dae036e33c0c5e77547b6febd498c0ea5b0d0d0c
César Alberca <cesalberca@gmail.com>
2017-02-08 17:47:52 +0100

    Test hibernate

--- src/main/java/modelo/GestorModelos.java
+++ src/main/java/modelo/GestorModelos.java
@@ -1,4 +1,19 @@
 package modelo;
 
+import modelo.entidades.Marca;
+import org.hibernate.Session;
+import utils.HibernateUtil;
+
 public class GestorModelos {
+
+    private Session session = HibernateUtil.getSessionFactory().openSession();
+
+    public GestorModelos() {
+        this.session.beginTransaction();
+        Marca marca = new Marca("Test");
+        this.session.save(marca);
+        System.out.println(marca);
+        this.session.getTransaction().commit();
+        System.out.println("Guardado");
+    }
 }
