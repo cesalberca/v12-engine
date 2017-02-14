e4e5ae4cac5f70c3719b516f62aba95395d2803f
César Alberca <cesalberca@gmail.com>
2017-02-14 17:29:33 +0100

    Creado método para conseguir todas las eficiencias

---
+++ src/main/java/persistencia/EficienciaPersistencia.java
@@ -0,0 +1,43 @@
+package persistencia;
+
+import modelo.entidades.Eficiencia;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateUtil;
+
+import java.util.List;
+
+public class EficienciaPersistencia implements EficienciaDao {
+    @Override
+    public List<Eficiencia> getTodasEficiencias() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        List<Eficiencia> listEficiencias = session.createQuery("FROM Eficiencia").list();
+
+        tx.commit();
+        session.close();
+
+        return listEficiencias;
+    }
+
+    @Override
+    public Eficiencia getEficiencia(int id) {
+        return null;
+    }
+
+    @Override
+    public void crearEficiencia(Eficiencia eficiencia) {
+
+    }
+
+    @Override
+    public void actualizarEficiencia(Eficiencia eficiencia) {
+
+    }
+
+    @Override
+    public void eliminarEficiencia(Eficiencia eficiencia) {
+
+    }
+}
--- src/main/java/persistencia/GestorPersistencia.java
+++ src/main/java/persistencia/GestorPersistencia.java
@@ -1,12 +1,13 @@
 package persistencia;
 
-import utils.HibernateUtil;
+import modelo.entidades.Eficiencia;
 
 public class GestorPersistencia {
 
     public GestorPersistencia(){
+        EficienciaPersistencia eficienciaPersistencia = new EficienciaPersistencia();
+        for (Eficiencia eficiencia : eficienciaPersistencia.getTodasEficiencias()) {
+            System.out.println(eficiencia);
+        }
     }
-
-
-
 }
