388e4d2bf3528b5eb51b639c8404db20dd4d3913
César Alberca <cesalberca@gmail.com>
2017-02-15 19:22:56 +0100

    Formato

--- src/main/java/persistencia/MarcaDao.java
+++ src/main/java/persistencia/MarcaDao.java
@@ -4,11 +4,7 @@ import modelo.entidades.Marca;
 
 import java.util.List;
 
-/**
- * Created by allen on 14/02/2017.
- */
 public interface MarcaDao {
-
     List<Marca> getTodasMarcas();
     Marca getMarca(int id);
     void crearMarca(Marca marca);
