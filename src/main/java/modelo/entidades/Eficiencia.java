aac6fb6d63c0dbf0fc331530007097d5a571cc4a
César Alberca <cesalberca@gmail.com>
2017-02-15 20:16:47 +0100

    Quitado lombok

--- src/main/java/modelo/entidades/Eficiencia.java
+++ src/main/java/modelo/entidades/Eficiencia.java
@@ -1,12 +1,10 @@
 package modelo.entidades;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Collection;
 
-@Data
 @Entity
 @Table(name = "eficiencia")
 public class Eficiencia implements Serializable {
@@ -32,8 +30,63 @@ public class Eficiencia implements Serializable {
         this.imagen = imagen;
     }
 
-    public Eficiencia(int id, String nombre) {
-        this.id = id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Eficiencia)) return false;
+
+        Eficiencia that = (Eficiencia) o;
+
+        if (id != that.id) return false;
+        if (nombre != null ? !nombre.equals(that.nombre) : that.nombre != null) return false;
+        if (!Arrays.equals(imagen, that.imagen)) return false;
+        return modelos != null ? modelos.equals(that.modelos) : that.modelos == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nombre != null ? nombre.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(imagen);
+        result = 31 * result + (modelos != null ? modelos.hashCode() : 0);
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+    public Collection<Modelo> getModelos() {
+        return modelos;
+    }
+
+    public void setModelos(Collection<Modelo> modelos) {
+        this.modelos = modelos;
+    }
+
+    @Override
+    public String toString() {
+        return "Eficiencia{" +
+            "id=" + id +
+            ", nombre='" + nombre + '\'' +
+            ", imagen=" + Arrays.toString(imagen) +
+            ", modelos=" + modelos +
+            '}';
     }
 }
--- src/main/java/modelo/entidades/Marca.java
+++ src/main/java/modelo/entidades/Marca.java
@@ -1,14 +1,10 @@
 package modelo.entidades;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
 
 @Entity
-@Data
 @Table(name = "marca")
 public class Marca implements Serializable {
     @Id
@@ -28,5 +24,50 @@ public class Marca implements Serializable {
     public Marca(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Marca)) return false;
+
+        Marca marca = (Marca) o;
+
+        if (id != marca.id) return false;
+        if (nombre != null ? !nombre.equals(marca.nombre) : marca.nombre != null) return false;
+        return modelos != null ? modelos.equals(marca.modelos) : marca.modelos == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nombre != null ? nombre.hashCode() : 0);
+        result = 31 * result + (modelos != null ? modelos.hashCode() : 0);
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Collection<Modelo> getModelos() {
+        return modelos;
+    }
+
+    public void setModelos(Collection<Modelo> modelos) {
+        this.modelos = modelos;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca{" +
+            "id=" + id +
+            ", nombre='" + nombre + '\'' +
+            ", modelos=" + modelos +
+            '}';
     }
 }
