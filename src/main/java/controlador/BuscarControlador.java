8aca48e011a90411fdf750fc0fd2688430441861
galaterro <adrix1992@gmail.com>
2017-02-21 16:40:34 +0100

    Buscar y BuscarControlador analizados

--- src/main/java/controlador/BuscarControlador.java
+++ src/main/java/controlador/BuscarControlador.java
@@ -3,9 +3,15 @@ package controlador;
 import persistencia.GestorPersistencia;
 import vista.Buscar;
 
+import java.awt.*;
+import java.awt.event.ActionListener;
+
 public class BuscarControlador {
     private Buscar buscar;
     private GestorPersistencia gestorPersistencia;
+    private ActionListener buscarListener, marcaSeleccionadaListener, marcaDeseleccionadoListener,
+    consumoSeleccionadoListener, consumoDeseleccionadoListener, emisionSeleccionadoListener, emisionDeseleccionadoListener,
+    clasificacionSeleccionadoListener, clasificacionDeseleccioandoListener, cerrarListener;
 
     public BuscarControlador(Buscar buscar, GestorPersistencia gestorPersistencia) {
         this.buscar = buscar;
@@ -15,6 +21,34 @@ public class BuscarControlador {
     }
 
     private void iniciarListener() {
+        buscarListener = actionEvent -> buscar.cerrarDialogo();
+        buscar.getButtonOK().addActionListener(buscarListener);
 
+        marcaSeleccionadaListener = actionEvent -> buscar.onMarcaSeleccionado();
+        buscar.getCbMarca().addActionListener(marcaSeleccionadaListener);
+
+        marcaDeseleccionadoListener = actionEvent -> buscar.onMarcaDeseleccionado();
+        buscar.getCbMarca().addActionListener(marcaDeseleccionadoListener);
+
+        consumoSeleccionadoListener = actionEvent -> buscar.onConsumoSeleccionado();
+        buscar.getCbConsumo().addActionListener(consumoSeleccionadoListener);
+
+        consumoDeseleccionadoListener = actionEvent -> buscar.onConsumoDeseleccionado();
+        buscar.getCbConsumo().addActionListener(consumoDeseleccionadoListener);
+
+        emisionSeleccionadoListener = actionEvent -> buscar.onEmisionesSeleccionado();
+        buscar.getCbEmisiones().addActionListener(emisionSeleccionadoListener);
+
+        emisionDeseleccionadoListener = actionEvent -> buscar.onEmisionesDeseleccionado();
+        buscar.getCbEmisiones().addActionListener(emisionDeseleccionadoListener);
+
+        clasificacionSeleccionadoListener = actionEvent -> buscar.onClasificacionSeleccionado();
+        buscar.getCbClasificacion().addActionListener(clasificacionSeleccionadoListener);
+
+        clasificacionDeseleccioandoListener = actionEvent -> buscar.onClasificacionDeseleccionado();
+        buscar.getCbClasificacion().addActionListener(clasificacionDeseleccioandoListener);
+
+        cerrarListener = actionEvent -> buscar.cerrarDialogo();
+        buscar.getButtonCancel().addActionListener(cerrarListener);
     }
 }
--- src/main/java/vista/Buscar.java
+++ src/main/java/vista/Buscar.java
@@ -7,32 +7,76 @@ public class Buscar extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JComboBox cbbMarca;
+    private JCheckBox cbClasificacion;
+    private JCheckBox cbEmisiones;
+    private JCheckBox cbConsumo;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTable jtResultados;
+    private JCheckBox cbMarca;
 
     public Buscar() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
-
-        buttonOK.addActionListener(e -> onOK());
-        buttonCancel.addActionListener(e -> onCancel());
-
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
-    private void onOK() {
+    public void onMarcaSeleccionado(){
+        cbbMarca.setEnabled(true);
+    }
+    public void onConsumoSeleccionado(){
+        cbConsumo.setEnabled(true);
+    }
+    public void onEmisionesSeleccionado(){
+        cbEmisiones.setEnabled(true);
+    }
+    public void onClasificacionSeleccionado(){
+        cbClasificacion.setEnabled(true);
+    }
+    public void onMarcaDeseleccionado(){
+        cbbMarca.setEnabled(false);
+    }
+    public void onConsumoDeseleccionado(){
+        cbConsumo.setEnabled(false);
+    }
+    public void onEmisionesDeseleccionado(){
+        cbEmisiones.setEnabled(false);
+    }
+    public void onClasificacionDeseleccionado(){
+        cbClasificacion.setEnabled(false);
+    }
+
+    public JButton getButtonOK() {
+        return buttonOK;
+    }
+
+    public JButton getButtonCancel() {
+        return buttonCancel;
+    }
+
+    public JCheckBox getCbClasificacion() {
+        return cbClasificacion;
+    }
+
+    public JCheckBox getCbEmisiones() {
+        return cbEmisiones;
+    }
+
+    public JCheckBox getCbConsumo() {
+        return cbConsumo;
+    }
+
+    public JCheckBox getCbMarca() {
+        return cbMarca;
+    }
+
+    public void cerrarDialogo() {
         dispose();
     }
 
-    private void onCancel() {
-        dispose();
-    }
 
     //TODO Quitar esto de aquí
     public static void main(String[] args) {
