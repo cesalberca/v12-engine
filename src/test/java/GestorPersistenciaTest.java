46a1fa6b2612287633ff801d94054ba28d34a624
César Alberca <cesalberca@gmail.com>
2017-02-21 15:58:57 +0100

    Añadido junit

---
+++ src/test/java/GestorPersistenciaTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GestorPersistenciaTest {
+
+    @Test
+    public void test() {
+        String expected = "Hello, JUnit!";
+        String hello = "Hello, JUnit!";
+        assertEquals(hello, expected);
+    }
+}
