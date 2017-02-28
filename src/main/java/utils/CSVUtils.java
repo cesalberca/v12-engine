9da8e1faa735498977c51b17c079ce30102f2c29
Kevin Castillo <kcastilloescudero@gmail.com>
2017-02-28 16:46:15 +0100

    Añadidos comentarios

--- src/main/java/utils/CSVUtils.java
+++ src/main/java/utils/CSVUtils.java
@@ -4,13 +4,29 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 
+/**
+ * Utilidades generales para escribir CSV
+ */
 public class CSVUtils {
     private static final char DEFAULT_SEPARATOR = ',';
 
+    /**
+     * Escribe una línea
+     * @param w Escritor
+     * @param values Valores
+     * @throws IOException Error al escribir
+     */
     public static void writeLine(Writer w, List<String> values) throws IOException {
         writeLine(w, values, DEFAULT_SEPARATOR, ' ');
     }
 
+    /**
+     * Escribe una línea
+     * @param w Escritor
+     * @param values Valores
+     * @param separators Separador
+     * @throws IOException Error al escribir
+     */
     public static void writeLine(Writer w, List<String> values, char separators) throws IOException {
         writeLine(w, values, separators, ' ');
     }
@@ -23,6 +39,14 @@ public class CSVUtils {
         return result;
     }
 
+    /**
+     * Escribe una línea
+     * @param w Escritor
+     * @param values Valores
+     * @param separators Separador personalizado
+     * @param customQuote Símbolo de separación
+     * @throws IOException Error al escribir
+     */
     public static void writeLine(Writer w, List<String> values, char separators, char customQuote) throws IOException {
         boolean first = true;
 
